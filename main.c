5f5f02b1f41f93895494f624d9e455d1a76fa69b
mfoot123 <mfoot456@gmail.com>
2023-03-22 17:19:23 -0700

    added block printing

--- main.c
+++ main.c
@@ -20,8 +20,9 @@ INODE *root;
 
 int INODES_PER_BLOCK = 8, ibuf[256];
 
-
 #define BLKSIZE 1024
+
+char buf[BLKSIZE];
 
 int fd;             // opened vdisk for READ
 int inodes_block;   // inodes start block number
@@ -35,6 +36,18 @@ int n;
 char *disk = "diskimage";
 int dev = 0; // set this when opening disk
 
+INODE *iget(int dev, int ino)
+{
+  INODE *ip;
+  int i, block, offset;
+  
+  block = (ino-1)/8 + inodes_block; // disk block containing this inode
+  offset= (ino-1)%8; // which inode in this block
+  get_block(dev, block, buf);
+  ip = (INODE *)buf + offset;
+  return ip;
+}
+
 int get_block(int dev, int blk, char *buf)
 {   
    lseek(dev, blk*BLKSIZE, SEEK_SET);
@@ -46,25 +59,31 @@ int search(INODE *ip, char *name)
   // Chapter 11.4.4  int i; 
   // Exercise 6
 
-  struct ext2_dir_entry_2 *dp; // dir_entry pointer
-    char *cp; // char pointer
-    int blk = ip->i_block[0]; // get data block (assuming it's the first block)
-    char buf[BLKSIZE], temp[256];
-    get_block(fd, blk, buf); // get data block into buf[ ]
-    dp = (struct ext2_dir_entry_2 *)buf; // as dir_entry
-    cp = buf;
-    while(cp < buf + BLKSIZE){
-        strncpy(temp, dp->name, dp->name_len); // make name a string
-        temp[dp->name_len] = 0; // ensure NULL at end
-        // if names match
-        if (strcmp(temp, name) == 0) { 
-            // return inode number
-            return dp->inode;
-        }
-        cp += dp->rec_len; // advance cp by rec_len
-        dp = (struct ext2_dir_entry_2 *)cp; // pull dp to next entry
-    }
-    return 0; // name not found
+  char sbuf[BLKSIZE], temp[256];
+   DIR *dp;
+   char *cp;
+ 
+   // ASSUME only one data block i_block[0]
+   // YOU SHOULD print i_block[0] number here
+   get_block(dev, ip->i_block[0], sbuf);
+
+   dp = (DIR *)sbuf;
+   cp = sbuf;
+ 
+   while(cp < sbuf + BLKSIZE){
+       strncpy(temp, dp->name, dp->name_len);
+       temp[dp->name_len] = 0;
+      
+       printf("%4d %4d %4d %s\n", dp->inode, dp->rec_len, dp->name_len, temp);
+
+      if(strcmp(temp, name)==0){
+      printf("found\n");
+      return dp->inode;
+      }
+
+       cp += dp->rec_len;
+       dp = (DIR *)cp;
+   }
 }
 
 int show_dir(INODE *ip)
@@ -100,16 +119,16 @@ int mount_root()
 /*************************************************************************/
 int tokenize(char *pathname)
 {
-  // set the number of tokens
-  int ntoken = 0;
-  // divide path name into tokens seperated by " "
-  char *p = strtok(pathname, " ");
-  // while p is not NULL put p into name[]
-  while (p != NULL)
-  {
-    name[ntoken++] = p;
-    p = strtok(NULL, " ");
-  }
+    char *s;
+    strcpy(gpath, pathname); // copy pathname to global gpath[]
+    n = 0;
+    s = strtok(gpath, "/"); // tokenize the path
+    while (s)               // while there is a token
+    {
+        name[n++] = s;      // write the token to name[n]
+        s = strtok(0, "/"); // tokenize again
+    }
+    return n;
 } 
 
 // the start of the "showblock" program
@@ -120,8 +139,6 @@ int tokenize(char *pathname)
 int main(int argc, char *argv[])
 {
    // follow the steps here: https://eecs.wsu.edu/~cs360/LAB5.html
-   char buf[BLKSIZE];
-
    // open disk
    dev = open(disk, O_RDWR);
 
@@ -156,10 +173,10 @@ int main(int argc, char *argv[])
     mount_root();
 
     // Print contents of the root DIRectory
-    print(root);
+    //print(root);
    
     // Tokenize pathname into name[0], name[1],... name[n-1]
-    
+    tokenize(&argv[1]);
 
     INODE *ip = root;
      int  ino, blk, offset;
@@ -192,8 +209,26 @@ int main(int argc, char *argv[])
 
     // Extract information from ip-> as needed:
     // Print direct block numbers;
+    // direct = blocks 0 - 11
+    for(int i = 0; i < 12; i++)
+    {
+      int block = ip->i_block[i];
+      if(block){
+        get_block(dev, block, ibuf);
+
+        int i = 0;
+        while(ibuf[i] && i < 256){
+          printf("%d", ibuf[i]);
+          i++;
+        }
+      }
+      else
+      printf("No indirect block number %d", &i);
+
+    }
 
     // Print indirect block numbers;
+    // indirect = block 12
       int b12 = ip->i_block[12];
       if(b12){
         get_block(dev, b12, ibuf);
@@ -204,7 +239,20 @@ int main(int argc, char *argv[])
           i++;
         }
       }
+      else
+      printf("No indirect block number");
 
     //  Print double indirect block numbers, if any
+    int b13 = ip->i_block[13];
+      if(b13){
+        get_block(dev, b12, ibuf);
 
+        int i = 0;
+        while(ibuf[i] && i < 256){
+          printf("%d", ibuf[i]);
+          i++;
+        }
+      }
+      else
+      printf("No double indirect block number");
 }
