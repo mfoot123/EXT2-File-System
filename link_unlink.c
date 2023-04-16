670c8d26263230b2fdc46bc2460ef6c3719f4ca8
mfoot123 <Mfoot456@gmail.com>
2023-04-16 15:42:22 -0700

    link, unlink, and sym functions

--- link_unlink.c
+++ link_unlink.c
@@ -19,137 +19,234 @@
 
 
 */
-int link(char* oldFile, char* newFile){
-/*
-    (1). get the INODE of /a/b/c into memory: mip->minode[ ]
-                                               INODE of /a/b/c
-                                               dev,ino
-                                               .......
-    mip = path2inode(pathname);
-*/
-    MINODE *omip;
-    omip = path2inode(oldFile);
-    int oino = mip->ino;
-    int odev = mip->dev;
 
-    // (2). check /a/b/c is NOT a DIRectory (link to DIR is NOT allowed).
-    // mip = iget(dev, ino); ???????????????????????????
+int link(char* oldFile, char* newFile) 
+{
 
-    // Verify mip->INODE is a DIR // return error if DIR
-    if (S_ISDIR(omip->INODE.i_mode))
-    {
-        printf("%s: Is a directory\n", oldFile);
-        // release the mip
+    // variables
+    int oino, pino;
+    char oparent[64], ochild[64], nparent[64], nchild[64];
+    MINODE *omip, *pmip;
+    INODE *ip;
+
+    // (1). get the INODE of /a/b/c (oldFile) into memory
+    oino = getino(dev, oldFile);
+    if (oino == 0) {
+        printf("%s does not exist\n", oldFile);
+        return -1;
+    }
+    omip = iget(dev, oino);
+
+    // (2). check /a/b/c (oldFile) is NOT a DIRectory (link to DIR is NOT allowed)
+    if (S_ISDIR(omip->INODE.i_mode)) {
+        printf("%s is a directory, cannot link\n", oldFile);
         iput(omip);
         return -1;
     }
 
-    MINODE *newOMip;
-    newOMip = path2inode(newFile);
-    // Checks to see if file exists already
-    if (newOMip->ino != 0)
-    {
-        printf("%s: Exists\n", oldFile);
-        return -1; // Throws off an error
+    // (3). break up paramters into parent (/x/y), child (z)
+    strcpy(oparent, dirname(oldFile));
+    strcpy(ochild, basename(oldFile));
+    strcpy(nparent, dirname(newFile));
+    strcpy(nchild, basename(newFile));
+
+    // check /x/y  exists and is a DIR but 'z' does not yet exist in /x/y/
+    pino = getino(dev, nparent);
+    if (pino == 0) {
+        printf("%s does not exist\n", nparent);
+        iput(omip);
+        return -1;
     }
-    
-    // (3). break up paramter into parent (/x/y), child (z)
-    //      check /x/y  exists and is a DIR but 'z' does not yet exist in /x/y/
-    char *parent, child;
-    int pino;
-    MINODE *pmip;
-    strcpy(parent, dirname(newFile));
-    strcpy(child, basename(newFile));
-    pmip = path2inode(parent);
-    pino = pmip->ino;
+    pmip = iget(dev, pino);
+    if (!S_ISDIR(pmip->INODE.i_mode)) {
+        printf("%s is not a directory, cannot link\n", nparent);
+        iput(omip);
+        iput(pmip);
+        return -1;
+    }
+    if (search(pmip, nchild)) {
+        printf("%s already exists in %s\n", nchild, nparent);
+        iput(omip);
+        iput(pmip);
+        return -1;
+    }
 
     // (4). Add an entry [ino rec_len name_len z] to the data block of /x/y/
-    //      This creates /x/y/z, which has the SAME ino as that of /a/b/c
-    // (NOTE: both /a/b/c and /x/y/z must be on the SAME device;
-    //        link can not be across different devices).
-    enter_name(pmip, oino, child);
+    // Add an entry for nchild in nparent with the same inode number as omip
+    enter_child(pmip, oino, nchild);
 
     // (5). increment the i_links_count of INODE by 1
     omip->INODE.i_links_count++;
-    omip->dirty = 1;
+    omip->modified = 1;
 
-    // (6). write INODE back to disk
+    // (6). write back to disk   
+    iput(omip);
     iput(pmip);
-    iput(newOMip);
+
+    return 0;
 }
 
 
 // ===========================================================================
 // 2.                     HOW TO unlink
 //      unlink pathname
-int unlink(char *linkedFile)
+void unlink(char *pathname)
 {
-    // (1). get pathname's INODE into memory
-    MINODE *mip = iget(linkedFile);
-    int ino = mip->ino;
+    int ino, pino, i;
+    MINODE *mip, *pip;
+    char parent[256], child[256], temp[256];
 
-    // check if its a file or directory
-    if (S_ISDIR(mip->inode.i_mode))
-    {
-        printf("%s: Is a directory", linkedFile);
-        return -1;
+    // get the ino of the file to be unlinked
+    ino = getino(dev, pathname);
+
+    // check if file exists
+    if (ino == 0) {
+        printf("File does not exist.\n");
+        return;
     }
-    else if (S_ISREG(mip->inode.i_mode))
-    {
-        printf("%s: is a file", linkedFile);
-        char *parent = dirname(linkedFile);
-        char *child = basename(linkedFile);
 
-        MINODE *pmip = iget(linkedFile);
-        int pino = pmip->ino;
-        rm_child(pmip, ino, child);
-        pmip->dirty = 1;
-        iput(pmip);
+    // get the minode of the file
+    mip = iget(dev, ino);
+
+    // check if file is a directory
+    if (S_ISDIR(mip->INODE.i_mode)) {
+        printf("Cannot unlink a directory.\n");
+        iput(mip);
+        return;
     }
-    
-    // (2). verify it's a FILE (REG or LNK), can not be a DIR;
 
-    // (3). decrement INODE's i_links_count by 1;
+    // get the name of the parent directory and the basename of the file
+    strcpy(temp, pathname);
+    strcpy(parent, dirname(temp));
+    strcpy(temp, pathname);
+    strcpy(child, basename(temp));
 
-    // (4). if i_links_count == 0 ==> rm pathname by
+    // get the minode of the parent directory
+    pino = getino(dev, parent);
+    pip = iget(dev, pino);
 
-    //         deallocate its data blocks by:
+    // remove the file from the parent directory
+    rm_child(pip, child);
 
-    //      Write a truncate(INODE) function, which deallocates ALL the data blocks
-    //      of INODE. This is similar to printing the data blocks of INODE.
+    // update the parent directory's time fields
+    pip->INODE.i_mtime = time(0L);
+    pip->INODE.i_ctime = time(0L);
+    pip->modified = 1;
 
-    //         deallocate its INODE;
+    // decrement the link count of the file
+    mip->INODE.i_links_count--;
 
-    // (5). Remove childName = basename(pathname) from the parent directory by
+    // if link count is still greater than 0, just write the minode back to disk and return
+    if (mip->INODE.i_links_count > 0) {
+        mip->modified = 1;
+        iput(mip);
+        iput(pip);
+        return;
+    }
 
-    //         rm_child(parentInodePtr, childName)
+    // deallocate the file's blocks and inode
+    truncate(mip);
 
-    //      which is the SAME as that in rmdir or unlink file operations.
-    return 0;
+    // deallocate the inode
+    mip->INODE.i_mode = 0;
+    mip->modified = 1;
+    iput(mip);
+
+    // update the parent directory's size and link count
+    pip->INODE.i_links_count--;
+    //pip->INODE.i_size -= DIR_ENTRY_SIZE;
+    pip->modified = 1;
+
+    // write the parent directory's minode back to disk and return
+    iput(pip);
 }
+
+
 
 // 3. ======================== HOW TO symlink ================================
 //    symlink oldNAME  newNAME    e.g. symlink /a/b/c /x/y/z
 
 //    ASSUME: oldNAME has <= 60 chars, inlcuding the ending NULL byte.
 
-int symlink(char *oldName, char *newName)
+int symlink(char *oldName, char *newName) 
 {
-    // (1). verify oldNAME exists (either a DIR or a REG file)
-    // (2). creat a FILE /x/y/z
-    // (3). change /x/y/z's type to LNK (0120000)=(b1010.....)=0xA...
-    // (4). write the string oldNAME into the i_block[ ], which has room for 60 chars.
+  int ino, pino;
+  MINODE *mip, *pmip;
+  char oldname_copy[60], path_copy[64];
+  strcpy(path_copy, newName);
+  strcpy(oldname_copy, oldName);
+  
+  // Get the device of the root inode
+  dev = root->dev;
 
-    //      set /x/y/z file size = number of chars in oldName
+  // (1). verify oldNAME exists
+  if ((ino = getino(dev, oldname_copy)) == 0) {
+    printf("%s does not exist\n", oldname_copy);
+    return -1;
+  }
+  mip = iget(dev, ino);
 
-    // (5). write the INODE of /x/y/z back to disk.
-    return 0;
+  // and is not a directory
+  if (S_ISDIR(mip->INODE.i_mode)) {
+    printf("%s is a directory\n", oldname_copy);
+    iput(mip);
+    return -1;
+  }
+  iput(mip);
+
+  // (2). creat a FILE /x/y/z
+  if (my_creat(newName, 0) < 0) {
+    printf("Failed to create %s\n", newName);
+    return -1;
+  }
+
+  // get the inode of the new file
+  ino = getino(dev, newName);
+  mip = iget(dev, ino);
+
+  // (3). change /x/y/z's type to LNK (0120000)=(b1010.....)=0xA...
+  mip->INODE.i_mode = 0xA000;
+
+  // (4). write the string oldNAME into the i_block[ ], which has room for 60 chars.
+  strncpy(mip->INODE.i_block, oldname_copy, 60);
+
+  // set /x/y/z file size = number of chars in oldName
+  mip->INODE.i_size = strlen(oldname_copy);
+
+  // (5). Write the INODE of /x/y/z back to disk.
+  mip->modified = 1;
+  iput(mip);
+  
+  return 0;
 }
 
+
 // 4. readlink pathname: return the contents of a symLink file
-int readlink(char* newName){
-// (1). get INODE of pathname into a minode[ ].
-// (2). check INODE is a symbolic Link file.
-// (3). return its string contents in INODE.i_block[ ].
-    return 0;
+int readlink(char *pathname, char *link) {
+
+  // (1). get INODE of pathname into a minode[ ].
+  int ino = path2inode(pathname);
+  if (ino == 0) {
+    // Path does not exist
+    return -1;
+  }
+  MINODE *mip = iget(dev, ino);
+
+  // (2). check INODE is a symbolic Link file.
+  if (!S_ISLNK(mip->INODE.i_mode)) {
+    printf("Not a symbolic link file");
+    iput(mip);
+    return -1;
+  }
+
+  // (3). return its string contents in INODE.i_block[ ].
+  int n = mip->INODE.i_size;
+  char buf[n+1];
+  buf[n] = '\0';   // Add null terminator
+  get_block(mip->dev, mip->INODE.i_block[0], buf);  // Read symbolic link file's block
+  strcpy(link, buf);
+
+  // Release the minode and return success
+  iput(mip);
+  return 0;
 }
