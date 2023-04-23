226fc9900c2324b744d33d2e416d853ef039dabd
mfoot123 <Mfoot456@gmail.com>
2023-04-23 14:29:38 -0700

    implemented myread()

--- read.c
+++ read.c
@@ -23,7 +23,72 @@ int read_file()
 
 
 
-int myread(int fd, char *buf, int nbytes)
+int myread(int fd, char *buf, int nbytes) 
 {
+    // variables:
+    int count = 0;
+    int avil, blk, remain;
+    char *cq = buf;
+    char readbuf[BLKSIZE];
 
+    // get offset from file descriptor
+    int offset = running->fd[fd]->offset;
+    // get fileSize from the i_size of the INODE of inodeptr
+    int fileSize = running->fd[fd]->inodeptr->INODE.i_size;
+
+    // calculate avil
+    avil = fileSize - offset;
+
+    while (nbytes && avil) 
+    {
+        // Compute LOGICAL BLOCK number lbk and startByte in that block from offset;
+        int lbk = offset / BLKSIZE;
+        int startByte = offset % BLKSIZE;
+        
+        // Map logical block to physical block
+        if (lbk < 12) {  // direct blocks
+            blk = running->fd[fd]->inodeptr->INODE.i_block[lbk];
+        } else if (lbk >= 12 && lbk < 256 + 12) {  // indirect blocks
+            // get the block number of the indirect block
+            get_block(running->fd[fd]->inodeptr->dev, running->fd[fd]->inodeptr->INODE.i_block[12], readbuf);
+            int *indirect = (int *)readbuf;
+            blk = indirect[lbk - 12];
+        } else {  // double indirect blocks
+            // get the block number of the double indirect block
+            get_block(running->fd[fd]->inodeptr->dev, running->fd[fd]->inodeptr->INODE.i_block[13], readbuf);
+            int *indirect2 = (int *)readbuf;
+            // get the block number of the indirect block
+            get_block(running->fd[fd]->inodeptr->dev, indirect2[(lbk - 256 - 12) / 256], readbuf);
+            int *indirect = (int *)readbuf;
+            // get the block number of the data block
+            blk = indirect[(lbk - 256 - 12) % 256];
+        }
+        
+        // get the data block into readbuf[BLKSIZE]
+        get_block(running->fd[fd]->inodeptr->dev, blk, readbuf);
+        
+        // copy from startByte to buf[ ], at most remain bytes in this block
+        char *cp = readbuf + startByte;
+        remain = BLKSIZE - startByte;   // number of bytes remain in readbuf[]
+        
+        while (remain > 0 && nbytes > 0 && avil > 0) {
+            *cq++ = *cp++;             // copy byte from readbuf[] into buf[]
+            offset++;           // advance offset 
+            count++;                  // inc count as number of bytes read
+            avil--; nbytes--;  remain--;
+            if (nbytes <= 0 || avil <= 0) break;  // finished reading requested bytes
+        }
+
+        // if one data block is not enough, loop back to OUTER while for more ...
+
+    }
+    
+    printf("myread: read %d bytes from file descriptor %d\n", count, fd);
+    
+    // updates offset
+    running->fd[fd]->offset = offset;
+    return count;
 }
+
+
+
