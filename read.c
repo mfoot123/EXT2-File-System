00f0a93c7c014c0abdc88139f000b7e661bef2b4
mfoot123 <Mfoot456@gmail.com>
2023-04-25 19:45:30 -0700

    modified read_file

--- read.c
+++ read.c
@@ -7,10 +7,6 @@ int read_file()
     int fd, nbytes;
     char buf[BLKSIZE];
 
-    // ask for a fd  and  nbytes to read;    e.g. read 0 10; read 0 123
-    printf("Enter fd and number of bytes to read: ");
-    scanf("%d %d", &fd, &nbytes);
-
     // verify fd is indeed opened for RD or RW;
     if (running->fd[fd]->mode != READ && running->fd[fd]->mode != READ_WRITE) {
         printf("error: fd is not open for RD or RW\n");
