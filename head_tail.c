e4ffb6f271398d9cadfc097b4e5080cae5d26b80
mfoot123 <Mfoot456@gmail.com>
2023-04-27 01:58:47 -0700

    improved tail and fixed head

--- head_tail.c
+++ head_tail.c
@@ -1,108 +1,89 @@
 #include "type.h"
 
-int head(char *pathname){
-    // 1. Open the file for read
+int head(char *pathname) 
+{
+    // Open the file for read
     int fd = open_file(pathname, READ); // Opens file to read
-    if(fd != READ){ // Makes sure it is open and for
+    if (fd < 0) {
+        perror("open");
         return -1;
     }
 
     char buf[BLKSIZE];
-    int links = 0;
-    char firstTenLines[BLKSIZE];
-
-    // 2. read BLKSIZE into char buf[] = |abcd..\nnxyzw...\n...
-    bzero(buf, BLKSIZE);
-    myread(fd, buf, BLKSIZE);
-
-    // 3. scan buf[] for \n; For each \n: linkes++ until 10; add a 0 after LAST \n
-    int i = 0;
-    while (links < 10 && i < BLKSIZE)
-    {
-        if (buf[i] == NULL)
-        {
-            break;
-        }
-        if(buf[i] == '\n'){
-            links++;
-        }
-        if (links == 10)
-        {
-            strncpy(firstTenLines, buf, i+1);
-            break;
-        }
-        
-        i++;
+    // Read BLKSIZE bytes into buf
+    int n = myread(fd, buf, BLKSIZE);
+    if (n < 0) {
+        perror("read");
+        close_file(fd);
+        return -1;
     }
 
-    //4. print buf as %s
+    // Scan buf for newline characters and count the number of lines
+    int lines = 0;
+    char *cp = buf;
+    while (*cp != '\0') {
+        if (*cp == '\n') {
+            lines++;
+            if (lines >= 10) {
+                *cp = '\0';
+                break;
+            }
+        }
+        cp++;
+    }
+
+    // Print buf as a string
     printf("=======================================================\n");
-    printf("%s\n", firstTenLines);
+    printf("%s\n", buf);
     printf("=======================================================\n");
-    bzero(buf, BLKSIZE);
+
     close_file(fd);
     return 0;
 }
 
-int tail(char *pathname){
-    
-    // 1. open file fore READ;
-    int fd = open_file(pathname, READ); // Opens file to read
-    if(fd != READ){ // Makes sure it is open and for
-        printf("ERROR: File Discriptor is not opened\n");
+int tail(char *pathname) {
+    // 1. Open file for read
+    int fd = open_file(pathname, READ);
+    if (fd < 0) {
+        printf("ERROR: Failed to open file.\n");
         return -1;
     }
 
-    // 2. get file_size (in its INODE.i_size)
+    // 2. Get file size (in its INODE.i_size)
     MINODE *mip = path2inode(pathname);
     int fileSize = mip->INODE.i_size;
-    if(fileSize < BLKSIZE)
-    {
-        printf("ERROR: FileSize is less than the block's\n");
+
+    // 3. Lseek to (file_size - BLKSIZE) (or to 0 if file_size < BLKSIZE)
+    long offset = fileSize >= BLKSIZE ? fileSize - BLKSIZE : 0;
+    myLSeek(fd, offset);
+
+    // 4. Read BLKSIZE into buf[]
+    char buf[BLKSIZE + 1];
+    int n = myread(fd, buf, BLKSIZE);
+    if (n < 0) {
+        printf("ERROR: Failed to read file.\n");
         return -1;
     }
+    buf[n] = '\0';
 
-    // 3. lssek to (file_size - BLKSIZE)      (OR to 0 if file_size < BLKSIZE)
-    myLSeek(fd, fileSize - BLKSIZE);
-
-    //lseek(mip->dev, fileSize - BLKSIZE, SEEK_SET);
-
-    //                                                                n
-    // 4. n = read BLKSIZE into buf[ ]=|............abc\n1234\nlast\n|0|
-    //                                                                |
-    //                                                         char *cp
-    char buf[BLKSIZE];
-    int links = 0;
-    char firstTenLines[BLKSIZE];
-    bzero(buf, BLKSIZE);
-    myread(fd, buf, BLKSIZE);
-
-    // 5. scan buf[ ] backwards for \n;  lines++  until lines=11
-    int i = fileSize;
-    while (links < 11)
-    {
-        if(buf[i] == NULL)
-        {
-            break;
-        }
-        if (buf[i] == '\n')
-        {
-            links++;
-        }
-        if (links == 11)
-        {
-            strncpy(firstTenLines, buf, i + 1);
-            break;
+    // 5. Scan buf[] backwards for \n; lines++ until lines=11
+    int lines = 0, i = n - 1;
+    while (i >= 0 && lines < 11) {
+        if (buf[i] == '\n') {
+            lines++;
         }
         i--;
     }
 
-    // 6. print from cp+1 as %s
+    // 6. Print from cp+1 as %s
     printf("=======================================================\n");
-    printf("%s\n", firstTenLines);
+    printf("%s", &buf[i + 1]);
     printf("=======================================================\n");
 
-    bzero(buf, BLKSIZE);
+    // Clean up
     close_file(fd);
+    iput(mip);
+
     return 0;
 }
+
